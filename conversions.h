949a3ba8463bd5011c7d8d35ff3a87bb91af1ad6
mChasengnou <blueraverenergy@yahoo.com>
2016-02-19 09:30:03 -0800

    conversions
    -files added for converting numbers

---
+++ conversions.h
@@ -0,0 +1,17 @@
+/*****************************************************************************/
+/*!
+\file   conversions.h
+\author Tarrant Mangasarian
+\par    email: tarrant.mangasarian\@digipen.edu
+\par    DigiPen login: tarrant.mangasarian
+\par    Course: GAM150
+\date   2/14/2016
+\brief
+This file contains the function prototypes for conversions
+include this if you have warnings with type conversions
+*/
+/*****************************************************************************/
+float DoubleToFloat(double d);
+int DoubleToInt(double d);
+int FloatToInt(float f);
+float IntToFloat(int i);
