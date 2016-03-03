5f9d86d954410da2e10ec8dbea772fc4bc522a8b
argoms <katana.argoms@gmail.com>
2016-03-03 15:31:11 -0800

    fixed LKG
    tarrant pls sync more

--- Enemy.h
+++ Enemy.h
@@ -4,8 +4,6 @@
 #define PI 3.1415926535897
 #define ENEMY_ROTATION_SPEED  (2.0f*PI + 1)
 
-
-
 enum
 {
   ENEMY_STATE_IDLE,
@@ -14,8 +12,8 @@ enum
 };
 
 GameObject* EnemyCreate(PhysicsObject* _physics, Sprite* _sprite, Entity* _entity, int _type,
-int enemyType, float chaseSpeed, float detectRange, float knockback, float attackCooldown, float attackCooldownLength, float attackWindup, float attackWindupLength,
-float attackRange, float attackKnockbackForce, int attackDamage, float projectileSpeed);
+	int enemyType, float chaseSpeed, float detectRange, float knockback, float attackCooldown, float attackCooldownLength, float attackWindup, float attackWindupLength,
+	float attackRange, float attackKnockbackForce, int attackDamage, float projectileSpeed);
 void EnemyInitialize(GameObject* _thisObject);
 void EnemySimulate(GameObject* _thisObject);
 void EnemySimulateAI(GameObject* _thisObject);
