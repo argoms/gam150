1a5b8ef344da87099095a395a61308d27a893234
mChasengnou <blueraverenergy@yahoo.com>
2016-03-03 22:03:09 -0800

    checkpoint 8

---
+++ SplashScreen.c
@@ -0,0 +1,40 @@
+/*!
+\file   DeathScreen.c
+\author James Do
+\par    email: j.do\@digipen.edu
+\brief
+Functions handling the splash screen
+*/
+#include "Graphics.h"
+#include "AEEngine.h"
+#include "LevelManager.h"
+#include "Text.h"
+#include <stdio.h>
+#include "Button.h"
+#include "SplashScreen.h"
+#include "Audio.h"
+
+
+void SplashScreenInit()
+{
+  TextInit();
+  GCreateSprite(0, 0, GCreateAnimation(1, GCreateTexture("splash.png"), GCreateMesh(800, 600, 1, 1), 1), 1);
+  TextCreateString("", -300, -50);
+  //printf("AAA");
+
+  Audio_AddSoundSample("SwordSwing1.ogg");
+  Audio_AddSoundSample("SwordClash1.ogg");
+  Audio_AddSoundSample("SwordSwing2.ogg");
+  Audio_AddSoundSample("ButtonClick.ogg");
+  Audio_AddSoundSample("ButtonClick2.ogg");
+}
+
+void SplashScreenRun()
+{
+  //printf("AAA");
+  if (AEInputCheckReleased(VK_SPACE))
+  {
+    LevelSetNext(level_mainMenu);
+    //Audio_PlaySoundSample("SwordSwing1.ogg", 0);
+  }
+}
