637cd224999cb0c9728eae767045d91f48ddb200
cfa44acepilot <ttmangasarian@gmail.com>
2016-04-12 16:53:14 -0700

    stats fixes stable, mostly
    stats changes

--- stats.h
+++ stats.h
@@ -1,4 +1,9 @@
 #pragma once
+//#include "Text.h"
+#include "Button.h"
+#include "AEEngine.h"
+#include <stdlib.h>
+
 typedef struct
 {
   int kills;                  // enemies killed
@@ -9,3 +14,22 @@ typedef struct
 }statistics;
 
 statistics* InitializePlayerStats();
+
+void IncrementKills(statistics* stats);
+void IncrementDeaths(statistics* stats);
+void SetTime(statistics* stats, float elapsed_time);
+void IncrementRoomsCleared(statistics* stats);
+void Incrementhighest_level_reached(statistics* stats);
+
+void ResetKills(statistics* stats);
+void ResetDeaths(statistics* stats);
+void ResetRoomsCleared(statistics* stats);
+void Resethighest_level_reached(statistics* stats);
+void ResetTime(statistics* stats);
+
+
+float GetSeconds(statistics* stats);
+float GetMinutes(statistics* stats);
+float ResetStats(statistics* stats);
+
+void FreeStats(statistics* stats);
