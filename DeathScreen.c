c875558288d19b236d674c18c6ca8df5265fba5d
mChasengnou <blueraverenergy@yahoo.com>
2016-04-12 11:05:20 -0700

    finalized music
    -last touches to music

--- DeathScreen.c
+++ DeathScreen.c
@@ -71,7 +71,8 @@ void DeathScreenInit()
   float text_offset = 90.0f;
   TextString *main_menu_text;
   main_menu_text = TextCreateHUDString(" Menu", buttonx - text_offset, buttony);
-  AEGfxTexture* button_texture = GCreateTexture("animations/buttons/button_texture.png");
+  //TextStringSetTint(main_menu_text, GTint(1, 1, 1, 1));
+  AEGfxTexture* button_texture = GCreateTexture("animations/buttons/button_texture2.png");
   Animation* anim_button = GCreateAnimation(1,
     button_texture,   //was null
     button_mesh,
