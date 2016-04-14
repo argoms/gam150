498f8e4affd24930d436881e438f1b1f6bcfe541
argoms <katana.argoms@gmail.com>
2016-04-14 11:30:56 -0700

    file headers updated

--- EnemyHealthIndicator.h
+++ EnemyHealthIndicator.h
@@ -1,3 +1,13 @@
+/*!
+Project (working title): Epoch
+\file   EnemyHealthIndicator.h
+\author James Do
+\par    email: j.do\@digipen.edu
+\brief
+Functionality for displaying hearts representing enemy health above enemies.
+
+All content © 2016 DigiPen (USA) Corporation, all rights reserved.
+*/
 #include "GameObject.h"
 #include "GameLevel.h"
 #include "Entity.h"
