e0dca1feda9a76157e723233494a775f8708db2b
noah <h.noah@digipen.edu>
2016-04-14 10:45:59 -0700

    got rid of a printf
    no more frames

--- EnemyStateMachine.c
+++ EnemyStateMachine.c
@@ -12,7 +12,6 @@
 */
 void ESMachineRun(GameObject* enemy)
 {
-  printf("ENEMYAI RUNNING\n");
   EnemyContainer* enemyContainer = enemy->miscData;
   if (enemy->enemyAI->currentEnemyState != enemy->enemyAI->newEnemyState)
   {
